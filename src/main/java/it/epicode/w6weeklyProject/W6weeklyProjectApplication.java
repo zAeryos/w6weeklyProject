adfa7774ad622c90e8b5352bebd7f321a7e29586
zAeryos <dave_13_08@hotmail.it>
2024-02-16 13:28:35 +0100

    Initial commit

---
+++ src/main/java/it/epicode/w6weeklyProject/W6weeklyProjectApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.w6weeklyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W6weeklyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W6weeklyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/w6weeklyProject/W6weeklyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.w6weeklyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6weeklyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
